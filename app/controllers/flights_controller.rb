a6ac1d0168f4cea5f37873f9210d47695e1c303b
Dan Bateman <dbateman0319@starkstate.net>
2017-09-14 23:12:19 -0400

    Form for ticket finding created

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,6 @@
 class FlightsController < ApplicationController
 	def index
+		@airport_options = Airport.all.map{ |u| [ u.airport_code ] }
+		@flight_options = Flight.all.map{ |u| [ u.start ] }
 	end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,12 @@
 class Flight < ApplicationRecord
 	belongs_to :from_airport, :class_name => "Airport"
 	belongs_to :to_airport, :class_name => "Airport"
+
+	def self.get_starts
+		starts = Flight.find_by_sql("select distinct start from flights where start is not null")
+	end
+
+	def start_formatted
+		start.strftime("%m/%d/%Y")
+	end
 end
