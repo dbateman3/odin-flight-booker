69cac2ea0f65152dfaa3b87bf8e82f5f61a2ddf1
Dan Bateman <dbateman0319@starkstate.net>
2017-09-21 23:50:38 -0400

    Booking form done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,25 @@
 class BookingsController < ApplicationController
   def new
+  	@booking = Booking.new
+  	@num_passengers = params[:num_tickets]
+  	@num_passengers.to_i.times { @booking.passengers.build }
+  	@flight = Flight.find(params[:flight][:flight_id])
+  	
+
   end
+
+  def create
+  	@booking = Booking.create(booking_params)
+  	redirect_to @booking
+  end
+
+  def show
+  	@booking = Booking.find(params[:id])
+  end
+
+  private
+
+  	def booking_params
+  		params.require(:booking).permit(:flight_id, passengers: [:name, :email] )
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'flights#index'
   get 'flights/index'
+
+  resources :bookings
+  resources :passengers
   
 end
