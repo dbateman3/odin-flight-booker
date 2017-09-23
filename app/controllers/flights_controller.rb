b8e7c3632190065db2b5c57e363e19bbef635ea4
Dan Bateman <dbateman0319@starkstate.net>
2017-09-22 23:48:38 -0400

    Fixed some formatting

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,13 +1,13 @@
 class FlightsController < ApplicationController
 	def index
 		@airport_options = Airport.all.map{ |u| [ u.airport_code, u.id ] }
-		
+		@from_label = Airport.find(params[:from_code]).airport_code
+		@to_label = Airport.find(params[:to_code]).airport_code
 		@from_code = params[:from_code]
 		@to_code = params[:to_code]
 		@date = params[:date]
 		@num_tickets = params[:num_tickets]
 		@results = Flight.flight_results(@from_code, @to_code)
 		
-
 	end
 end
