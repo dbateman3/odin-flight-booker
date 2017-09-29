9c26c046d7d8d67e01b964e3748646bcb2a474c0
Dan Bateman <dbateman0319@starkstate.net>
2017-09-28 22:51:33 -0400

    Adding mailer

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,8 @@
 class FlightsController < ApplicationController
 	def index
 		@airport_options = Airport.all.map{ |u| [ u.airport_code, u.id ] }
-		@from_label = Airport.find(params[:from_code]).airport_code
-		@to_label = Airport.find(params[:to_code]).airport_code
+		
+		
 		@from_code = params[:from_code]
 		@to_code = params[:to_code]
 		@date = params[:date]
